6a631c702464fbbe77e21a87598bacab2d110c0c
Anna.Mkhitaryan <anna.mkhitaryan@acba.am>
2024-09-16 15:58:06 +0400

    homework

--- src/Homework/Arrays.java
+++ src/Homework/Arrays.java
@@ -19,21 +19,21 @@ public class Arrays {
         }  System.out.println("The maximum value in minMaxArray is " + max + ".");
         System.out.println("The minimum value in minMaxArray is " + min + ".");
 
-//        //2. delete array member
-//        int[] deleteMember = {1, 2, 3, 4};
-//        int[] alreadyDeleted = new int[deleteMember.length - 1];
-//        int j = 0;
-//
-//        for (int i : deleteMember) {
-//            if (deleteMember[i] != 2) {
-//                alreadyDeleted[j++] = deleteMember[i];
-//            }
-//        }
-//        System.out.println(java.util.Arrays.toString(alreadyDeleted));
-//
-//        //3. sum of 4 values
-//        int[] additives = new int[3];
-//        int sum = new Scanner(System.in).nextInt();
-//        int  additive0,  additive1,  additive2,  additive3;
+        //2. delete array member
+        int[] deleteMember = {1, 2, 3, 4};
+        int[] alreadyDeleted = new int[deleteMember.length - 1];
+        int j = 0;
+
+        for (int i : deleteMember) {
+            if (deleteMember[i] != 2) {
+                alreadyDeleted[j++] = deleteMember[i];
+            }
+        }
+        System.out.println(java.util.Arrays.toString(alreadyDeleted));
+
+        //3. sum of 4 values
+        int[] additives = new int[3];
+        int sum = new Scanner(System.in).nextInt();
+        int  additive0,  additive1,  additive2,  additive3;
     }
 }
