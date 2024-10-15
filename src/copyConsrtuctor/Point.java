71e2c7b67f56cefe4c8bde91f40fdccb67bd4115
Anna.Mkhitaryan <anna.mkhitaryan@acba.am>
2024-10-15 17:33:41 +0400

    copy constructor

---
+++ src/copyConsrtuctor/Point.java
@@ -0,0 +1,15 @@
+package copyConsrtuctor;
+
+public class Point {
+    private double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point other) {
+        this.x = other.x;
+        this.y = other.y;
+    }
+}
