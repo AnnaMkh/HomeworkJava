582f30e2f0dd8f2e27315953d0c761741465a68c
Anna.Mkhitaryan <anna.mkhitaryan@acba.am>
2024-10-17 15:24:32 +0400

    homework 1

--- src/Homework/Branching.java
+++ src/Homework/Branching.java
@@ -6,83 +6,83 @@ import java.util.Scanner;
 public class Branching {
     public static void main(String[] args) {
 
-        //increasing, decreasing,  niether
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input 3 numbers, please.");
-        int a1 = scanner.nextInt();
-        int a2 = scanner.nextInt();
-        int a3 = scanner.nextInt();
-
-        if (a1 < a2 && a2 < a3) {
-            System.out.println("Increasing");
-        } else if (a1 > a2 && a2 > a3) {
-            System.out.println("Decreasing");
-        } else System.out.println("Niether");
-
-        //leapYear
-        int year = scanner.nextInt();
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            System.out.println("Leap Year");
-        } else System.out.println("Non-leap year");
-
-        //calculate
-        System.out.println("Enter the character");
-        String character = scanner.next();
-        int result;
-        switch (character) {
-            case "*":
-                result = a1 * a2;
-                System.out.println(result);
-                break;
-            case ":":
-                result = a1 % a3;
-                System.out.println(result);
-                break;
-            case "+":
-                result = a1 + a2;
-                System.out.println(result);
-                break;
-            case "-":
-                result = a1 - a2;
-                System.out.println(result);
-                break;
-            default:
-                System.out.println("Character is not an operator");
-
-                // class list
-                String[] semesters = {"Fall", "Spring"};
-                String semester = semesters[new Random().nextInt(semesters.length)];
-
-                String[] courses = {"CS", "ES"};
-                String course = courses[new Random().nextInt(courses.length)];
-
-                String[] programs = {"Introduction to Computer Science", "English literature", "OOP", "Introduction to journalism"};
-
-                System.out.println(semester + " " + course);
-                switch (semester) {
-                    case ("Fall"):
-                        switch (course) {
-                            case ("CS"):
-                                String programFallCS = programs[0];
-                                System.out.println("In fall CS's program is " + programFallCS);
-                                break;
-                            case ("ES"):
-                                String programFallES = programs[2];
-                                System.out.println("In fall ES's program is " + programFallES);
-                                break;
-                        }
-                    case ("Spring"):
-                        switch (course) {
-                            case ("CS"):
-                                String programSpringCS = programs[1];
-                                System.out.println("In spring CS's program is " + programSpringCS);
-                                break;
-                            case ("ES"):
-                                String prograSpringES = programs[3];
-                                System.out.println("In spring ES's program is " + prograSpringES);
-                                break;
-                        }
-                }
-        }
+//        //increasing, decreasing,  niether
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Input 3 numbers, please.");
+//        int a1 = scanner.nextInt();
+//        int a2 = scanner.nextInt();
+//        int a3 = scanner.nextInt();
+//
+//        if (a1 < a2 && a2 < a3) {
+//            System.out.println("Increasing");
+//        } else if (a1 > a2 && a2 > a3) {
+//            System.out.println("Decreasing");
+//        } else System.out.println("Niether");
+//
+//        //leapYear
+//        int year = scanner.nextInt();
+//        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+//            System.out.println("Leap Year");
+//        } else System.out.println("Non-leap year");
+//
+//        //calculate
+//        System.out.println("Enter the character");
+//        String character = scanner.next();
+//        int result;
+//        switch (character) {
+//            case "*":
+//                result = a1 * a2;
+//                System.out.println(result);
+//                break;
+//            case ":":
+//                result = a1 % a3;
+//                System.out.println(result);
+//                break;
+//            case "+":
+//                result = a1 + a2;
+//                System.out.println(result);
+//                break;
+//            case "-":
+//                result = a1 - a2;
+//                System.out.println(result);
+//                break;
+//            default:
+//                System.out.println("Character is not an operator");
+//
+//                // class list
+//                String[] semesters = {"Fall", "Spring"};
+//                String semester = semesters[new Random().nextInt(semesters.length)];
+//
+//                String[] courses = {"CS", "ES"};
+//                String course = courses[new Random().nextInt(courses.length)];
+//
+//                String[] programs = {"Introduction to Computer Science", "English literature", "OOP", "Introduction to journalism"};
+//
+//                System.out.println(semester + " " + course);
+//                switch (semester) {
+//                    case ("Fall"):
+//                        switch (course) {
+//                            case ("CS"):
+//                                String programFallCS = programs[0];
+//                                System.out.println("In fall CS's program is " + programFallCS);
+//                                break;
+//                            case ("ES"):
+//                                String programFallES = programs[2];
+//                                System.out.println("In fall ES's program is " + programFallES);
+//                                break;
+//                        }
+//                    case ("Spring"):
+//                        switch (course) {
+//                            case ("CS"):
+//                                String programSpringCS = programs[1];
+//                                System.out.println("In spring CS's program is " + programSpringCS);
+//                                break;
+//                            case ("ES"):
+//                                String prograSpringES = programs[3];
+//                                System.out.println("In spring ES's program is " + prograSpringES);
+//                                break;
+//                        }
+//                }
+//        }
     }
 }
--- src/Homework/Recursion.java
+++ src/Homework/Recursion.java
@@ -2,101 +2,101 @@ package Homework;
 
 public class Recursion {
     public static void main(String[] args) {
-
-        //2 fibonacci
-        int n = 5;
-        System.out.println(fibonacci(n));
-
-        //3 factorial
-        int n1 = 6;
-        System.out.println(factorial(n1));
-
-        //4 power
-        int n2 = 5;
-        int k = 3;
-        System.out.println(power(n, k));
-
-        //5 Min max from array-recursion
-        int[] arrayRecursion = {15, 20, 35, 1, -45, -80};
-        System.out.println(findMinElement(arrayRecursion, arrayRecursion.length - 1));
-        System.out.println(findMaxElement(arrayRecursion, arrayRecursion.length - 1));
-
-        //6 revert array
-        printReversedArray(arrayRecursion, arrayRecursion.length - 1);
-        System.out.println();
-
-        //7 poliandrome
-        System.out.println(poliandrome("Hello"));
-        System.out.println(poliandrome("ol111lo"));
-
-        //8
-        int a = 1555;
-        System.out.println(sumOfDogits(12586665));
-    }
-//-----------------------------------------------------------------------------------------------------------------
-
-   public static int sumOfDogits(int n){
-        if (n == 0)
-            return 0;
-        return (n % 10 + sumOfDogits(n / 10));
-    }
-
-    public static boolean poliandrome(String word) {
-        if (word.length() == 0 || word.length() == 1) {
-            return true;
-        }
-        if (word.charAt(0) == word.charAt(word.length() - 1)) {
-            return poliandrome(word.substring(1, word.length() - 1));
-        }
-        return false;
-    }
-
-    public static void printReversedArray(int[] array, int nnn) {
-        if (nnn == 0) {
-            return;
-        }
-        System.out.print(array[nnn] + " ");
-        printReversedArray(array, nnn - 1);
-    }
-
-    public static int findMinElement(int[] arrayRecursion, int nn) {
-        if (nn == 0) {
-            return arrayRecursion[nn];
-        }
-        int minOfArray = findMinElement(arrayRecursion, nn - 1);
-        return Math.min(arrayRecursion[nn], minOfArray);
-    }
-
-    public static int findMaxElement(int[] arrayRecursion, int nn) {
-        if (nn == 0) {
-            return arrayRecursion[nn];
-        }
-        int maxOfArray = findMaxElement(arrayRecursion, nn - 1);
-        return Math.max(arrayRecursion[nn], maxOfArray);
-    }
-
-    public static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        int nfib = fibonacci(n - 1) + fibonacci(n - 2);
-        return nfib;
-    }
-
-    public static int factorial(int n1) {
-        if (n1 <= 1) {
-            return n1;
-        }
-        int fact = n1 * factorial(n1 - 1);
-        return fact;
-    }
-
-    public static int power(int n2, int k) {
-        if (k <= 1) {
-            return k;
-        }
-        k--;
-        int power = n2 * power(n2, k);
-        return power;
-    }
+//
+//        //2 fibonacci
+//        int n = 5;
+//        System.out.println(fibonacci(n));
+//
+//        //3 factorial
+//        int n1 = 6;
+//        System.out.println(factorial(n1));
+//
+//        //4 power
+//        int n2 = 5;
+//        int k = 3;
+//        System.out.println(power(n, k));
+//
+//        //5 Min max from array-recursion
+//        int[] arrayRecursion = {15, 20, 35, 1, -45, -80};
+//        System.out.println(findMinElement(arrayRecursion, arrayRecursion.length - 1));
+//        System.out.println(findMaxElement(arrayRecursion, arrayRecursion.length - 1));
+//
+//        //6 revert array
+//        printReversedArray(arrayRecursion, arrayRecursion.length - 1);
+//        System.out.println();
+//
+//        //7 poliandrome
+//        System.out.println(poliandrome("Hello"));
+//        System.out.println(poliandrome("ol111lo"));
+//
+//        //8
+//        int a = 1555;
+//        System.out.println(sumOfDogits(12586665));
+//    }
+////-----------------------------------------------------------------------------------------------------------------
+//
+//   public static int sumOfDogits(int n){
+//        if (n == 0)
+//            return 0;
+//        return (n % 10 + sumOfDogits(n / 10));
+//    }
+//
+//    public static boolean poliandrome(String word) {
+//        if (word.length() == 0 || word.length() == 1) {
+//            return true;
+//        }
+//        if (word.charAt(0) == word.charAt(word.length() - 1)) {
+//            return poliandrome(word.substring(1, word.length() - 1));
+//        }
+//        return false;
+//    }
+//
+//    public static void printReversedArray(int[] array, int nnn) {
+//        if (nnn == 0) {
+//            return;
+//        }
+//        System.out.print(array[nnn] + " ");
+//        printReversedArray(array, nnn - 1);
+//    }
+//
+//    public static int findMinElement(int[] arrayRecursion, int nn) {
+//        if (nn == 0) {
+//            return arrayRecursion[nn];
+//        }
+//        int minOfArray = findMinElement(arrayRecursion, nn - 1);
+//        return Math.min(arrayRecursion[nn], minOfArray);
+//    }
+//
+//    public static int findMaxElement(int[] arrayRecursion, int nn) {
+//        if (nn == 0) {
+//            return arrayRecursion[nn];
+//        }
+//        int maxOfArray = findMaxElement(arrayRecursion, nn - 1);
+//        return Math.max(arrayRecursion[nn], maxOfArray);
+//    }
+//
+//    public static int fibonacci(int n) {
+//        if (n <= 1) {
+//            return n;
+//        }
+//        int nfib = fibonacci(n - 1) + fibonacci(n - 2);
+//        return nfib;
+//    }
+//
+//    public static int factorial(int n1) {
+//        if (n1 <= 1) {
+//            return n1;
+//        }
+//        int fact = n1 * factorial(n1 - 1);
+//        return fact;
+//    }
+//
+//    public static int power(int n2, int k) {
+//        if (k <= 1) {
+//            return k;
+//        }
+//        k--;
+//        int power = n2 * power(n2, k);
+//        return power;
+   }
 }
