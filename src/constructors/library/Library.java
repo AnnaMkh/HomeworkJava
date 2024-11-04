8e5a83cb6cf38685256cf9ee2c64bcfb8c5e1f56
Anna.Mkhitaryan <anna.mkhitaryan@acba.am>
2024-11-04 14:27:22 +0400

    Library

--- src/constructors/library/Library.java
+++ src/constructors/library/Library.java
@@ -1,7 +1,33 @@
 package constructors.library;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Library {
-    Book[] books;
+    private ArrayList<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public Book getBook(String bookName) {
+        for (Book book : books) {
+            if (book.getAuthor().equals(bookName)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public String[] getBookNames() {
+        String[] bookNames = new String[books.size()];
+        for (int i = 0; i < books.size(); i++) {
+            bookNames[i] = books.get(i).getNameOfBook();
+
+        }
+        return bookNames;
+    }
 }
